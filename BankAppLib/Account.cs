b5d3f21503cda178e14574dd0d7f42601d232954
MaheuTaroo <pantupa@gmail.com>
2020-10-26 22:36:26 +0000

    Criação das componentes gráficas e de parte da classe base de contas

---
+++ BankAppLib/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace BankAppLib
+{
+    public abstract class Account
+    {
+        public abstract int Amount { get; set; }
+        public Account(Guid guid)
+        {
+            
+        }
+        public abstract bool Deposit(int account);
+        public abstract bool Withdraw(int amount);
+    }
+}
